1609d43ae47bd2d42c9e236acbad34e1ea35e251
gyanoday kothari <gyanoday06@gmail.com>
2023-09-18 08:32:43 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Header />
         <div className="content">
           <Routes>
-            <Route exact path="/space-tour" element={<Home />} />
+            <Route exact path="/home" element={<Home />} />
             <Route path="/destinations" element={<Destinations />} />
             <Route path="/crew" element={<Crew />} />
             <Route path="/technology" element={<Technology />} />
