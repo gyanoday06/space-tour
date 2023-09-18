fa272969895fb7cb30dc3f144330cc5f58397637
gyanoday kothari <gyanoday06@gmail.com>
2023-09-18 08:48:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Import 'Navigate' from react-router-dom
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import Home from './components/home';
 import Destinations from './components/destinations';
@@ -9,17 +9,13 @@ import Technology from './components/technology';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    window.location.pathname === '/' || Navigate('/');
-  }, []);
-
   return (
     <Router>
       <div className="App">
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route exact path="/" element={<Home />} />
             <Route path="/destinations" element={<Destinations />} />
             <Route path="/crew" element={<Crew />} />
             <Route path="/technology" element={<Technology />} />
